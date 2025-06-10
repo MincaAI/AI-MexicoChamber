fd4c4a59b1d592b0b921594d7481d5cb33249a76
MincaAI <contactmincaai@gmail.com>
2025-06-10 17:53:07 +0200

    anthorpic

--- Agent/agent2005.py
+++ Agent/agent2005.py
@@ -38,7 +38,7 @@ class StreamPrintCallback(BaseCallbackHandler):
 # === 1. Charger les variables d'environnement ===
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX")
 
